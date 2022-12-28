e2106d2f11093fd2ae10596069318d9aa642a18b
dahmed <DhulkiflA@Gmail.com>
2022-12-29 10:49:13 +1300

    Add div container to the js-file.js

--- lesson7/js-file.js
+++ lesson7/js-file.js
@@ -12,3 +12,14 @@ Redtext.textContent = "Hey I'm red!";
 Redtext.style.color = 'red';
 
 container.appendChild(Redtext);
+
+const Heading3 = document.createElement('h3');
+Heading3.classList.add('h3');
+Heading3.textContent = "I'm a blue h3!";
+Heading3.style.color = 'blue';
+
+container.appendChild(Heading3);
+
+const container2 = document.createElement('div');
+container2.classList.add('div')
+container2.style.cssText = 'background: pink; border: black;';
