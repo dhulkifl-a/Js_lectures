6489739898d9fea30a1758303912d37251dfaa0a
dahmed <DhulkiflA@Gmail.com>
2022-12-28 13:58:31 +1300

    Add new <p> container with red text

--- lesson7/js-file.js
+++ lesson7/js-file.js
@@ -5,3 +5,10 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+const Redtext = document.createElement('p');
+Redtext.classList.add('p');
+Redtext.textContent = "Hey I'm red!";
+Redtext.style.color = 'red';
+
+container.appendChild(Redtext);
