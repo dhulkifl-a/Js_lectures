315851563d1b7dbfb52cd1337fbb5ce6b962f3ee
dahmed <DhulkiflA@Gmail.com>
2022-12-29 19:04:50 +1300

    Add boreder to div

--- lesson7/js-file.js
+++ lesson7/js-file.js
@@ -20,6 +20,19 @@ Heading3.style.color = 'blue';
 
 container.appendChild(Heading3);
 
-const container2 = document.createElement('div');
-container2.classList.add('div')
-container2.style.cssText = 'background: pink; border: black;';
+const content2 = document.createElement('div');
+content2.classList.add('div')
+content2.style.cssText = "background-color: pink; border:2px solid black;";
+
+const Heading1 = document.createElement('h1');
+Heading1.classList.add('h1');
+Heading1.textContent = "I'm in a div"
+
+const ptext = document.createElement('p');
+ptext.classList.add('p');
+ptext.textContent = "ME TOO!";
+
+content2.appendChild(Heading1)
+content2.appendChild(ptext)
+
+container.appendChild(content2)
